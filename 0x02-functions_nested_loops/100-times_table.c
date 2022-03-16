8ca8119c4c04a4b82f15a7194ac64a106e5dc16b
Rediet Solomon <redisolomon2@gmail.com>
2022-03-16 03:49:05 +0300

    Advanced

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,61 +1,18 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- *print_times_table - prints timestable from n
+ * main - check the code.
  *
- *@n:input
- *
- *Return: void
- *
+ * Return: Always 0.
  */
-void print_times_table(int n)
+int main(void)
 {
-	int result;
-
-	if (n <= 15 && n > 0)
-	{
-		int i;
-
-		for (i = 0; i <= n; i++)
-		{
-			int j;
-
-			for (j = 0; j <= n; j++)
-			{
-				result = i * j;
-				if (j == 0)
-				{
-					_putchar('0' + result);
-				}
-				else if (result >= 10)
-				{
-					_putchar(' ');
-					_putchar('0' + (result / 10));
-					_putchar('0' + (result % 10));
-				}
-				else if (result >= 100)
-				{
-					_putchar(' ');
-					_putchar('0' + (result / 100));
-					_putchar('0' + ((result / 10) % 10));
-					_putchar('0' + (result % 10));
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
-				}
-				if (j != n)
-				{
-					_putchar(',');
-				}
-			}
-			_putchar('\n');
-		}
-	}
-	else
-	{
-		_putchar('\n');
-	}
+    print_times_table(3);
+    _putchar('\n');
+    print_times_table(5);
+    _putchar('\n');
+    print_times_table(98);
+    _putchar('\n');
+    print_times_table(12);  
+    return (0);
 }
