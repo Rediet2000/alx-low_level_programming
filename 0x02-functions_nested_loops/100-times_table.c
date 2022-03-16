0f6aa07abd169f2d8c6e1bca55c6b1a807532600
Rediet2000 <Redisolomon2@gmail.com>
2022-03-15 19:09:59 -0700

    debugging

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,18 +1,51 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - check the code.
+ * print_times_table - This is a absolute value
+ *
+ * @n: This is the entry
  *
  * Return: Always 0.
  */
-int main(void)
+
+void print_times_table(int n)
 {
-    print_times_table(3);
-    _putchar('\n');
-    print_times_table(5);
-    _putchar('\n');
-    print_times_table(98);
-    _putchar('\n');
-    print_times_table(12);  
-    return (0);
+	if (n >= 0 && n <= 15)
+	{
+		int b, c, d;
+		for (c = 0; c <= n; c++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				d = b * c;
+
+				if (b == 0)
+					_putchar(d + '0');
+				else if (d <= 9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(d + '0');
+				}
+				else if (d >= 10 && d <= 99)
+				{
+					_putchar(32);
+					_putchar(((d / 10) % 10) + '0');
+					_putchar((d % 10) + '0');
+				}
+				else
+				{
+					_putchar((d / 100) + '0');
+					_putchar(((d / 10) % 10) + '0');
+					_putchar((d % 10) + '0');
+				}
+				if (b != n)
+				{
+					_putchar(',');
+					_putchar(32);
+				}
+			}
+		_putchar('\n');
+		}
+	}
 }
--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,3 +7,23 @@
  *Return: 0 (success)
  *
  */
+int main(void)
+{
+	unsigned long count, i, j,k, sums;
+	
+	i = sums = 0;
+	j = 1;
+	for (count = 0; count < 50; count++)
+ 	{ 
+		k = i + j;
+		i = j;
+		j = k;
+		if (k % 2 == 0 && k < 4000000)
+		{
+		sums += k;
+		}
+	}
+	printf("%lu\n", sums);
+	return (0);
+}
+
