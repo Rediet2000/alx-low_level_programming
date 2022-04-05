002317b4f9d840fc9f67021854618572d1e591db
Rediet Solomon <redisolomon2@gmail.com>
2022-04-05 21:29:04 +0300

    commiting malloc_free

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,97 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "holberton.h"
-/**
- *word_len - finds the length of a word
- *@str:string to test
- *
- *Return:int
- */
-int word_len(char *str)
-{
-	int i = 0, len = 0;
+#include <stdlib.h>
 
-	while (*(str + i) && *(str + i) != ' ')
-	{
-		len++;
-		i++;
-	}
-	return (len);
-}
 /**
- *word_count - counts the number of words
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: number of arguments
+ * @av: double pointer to arguments
  *
- *@str:input
- *
- *Return:(no. of words)
- *
+ * Return:pointer to new string, or NULL if error
  */
-int word_count(char *str)
+char *argstostr(int ac, char **av)
 {
-	int i = 0, len = 0, count = 0;
+	int i, j, k = 0, n = 0;
+	char *s;
 
-	for (i = 0; *(str + i); i++)
-	{
-		len++;
-	}
-	for (i = 0; i < len; i++)
-	{
-		if (*(str + i) != ' ')
-		{
-			count++;
-			i += word_len(str + i);
-		}
-	}
-	return (count);
-}
-/**
- *strtow - splits a string into words
- *
- *@str:input
- *
- *Return:0 - success
- *
- */
-char **strtow(char *str)
-{
-	int i, words, w, letters, l;
-	char **p;
-
-	if (str == NULL || str[0] == '\0')
-	{
+	if (ac <= 0 || av == NULL)
 		return (NULL);
-	}
-	words = word_count(str);
-	if (words == 0)
+	for (i = 0; i < ac; i++)
 	{
+		for (j = 0; av[i][j]; j++)
+			n++;
+		n++;
+	}
+	n++;
+	s = malloc(n * sizeof(char));
+	if (s == NULL)
 		return (NULL);
-	}
-	p = malloc(sizeof(char *) * (words + 1));
-	if (p == NULL)
+	for (i = 0; i < ac; i++)
 	{
-		return (NULL);
+		for (j = 0; av[i][j]; j++)
+		{
+			s[k] = av[i][j];
+			k++;
+		}
+		s[k] = '\n';
+		k++;
 	}
-	for (i = 0; i < words; i++)
-	{
-		while (*(str + w) == ' ')
-		{
-			w++;
-		}
-		letters = word_len(str + w);
-		p[i] = malloc(sizeof(char) * (letters + 1));
-		if (p[i] == NULL)
-		{
-			for (; i >= 0; i--)
-				free(p[i]);
-			free(p);
-			return (NULL);
-		}
-		for (l = 0; l < letters; l++)
-		{
-			p[i][l] = str[w++];
-		}
-		p[i][l] = '\0';
-	}
-	p[i] = NULL;
-	return (p);
+	s[k] = '\0';
+	return (s);
 }
